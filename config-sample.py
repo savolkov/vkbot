f05843b1fed8f3a84afd6b7afd728c2ae8f4d076
Semyon Volkov <savolkov_1@edu.hse.ru>
2019-01-21 12:14:43 +0500

    Refreshed config.sample file

--- config-sample.py
+++ config-sample.py
@@ -1,11 +1,44 @@
-ID_SELF = '' # user id
-USERNAME = '' # login
-PWD = '' # password
-APPID = '' # vk app id
+# Auth params
+
+# Bot's VK ID
+ID_SELF = ''
+# Bot's username
+USERNAME = ''
+# Bot's password
+PWD = ''
+# VK app ID
+APPID = ''
+
+# Config
+
+# Plugins directory
+plPath = 'plugins/'
+
+# API version?
 V = '5.92'
-# NSFW-чаты, если человек больше, чем:
+# Chat is NSFW if there are more than `nsfwCount` members
 nsfwCount = 15
+# Cooldown between !command usage
+cooldown = 15  # seconds
+# Timeout when failed to get longpoll params
+reconnectTimeout = 50  # seconds
+# Users with admin rights
+adminList = ['']
+# Whitelisted users
+whiteList = ['']
+
+# Phrase dictionaries
+
+# Warning when NSFW command is used in NSFW chat
 nsfwMessages = ['Я стесняюсь, тут слишком много людей...']
-voiceWarns = ['ГОЛОСОВЫЕ ЗАПРЕЩЕНЫ В ЭТОМ ЧАТЕ', 'МОЖЕТ ХВАТИТ, А', 'Слушай, я скоро из чата кикать буду таких да']
-helloMsgs = ['Здарова агалы', 'Доброго времени суток :-)']
-errorMsgs = ['Кхе кхе кххха ух подавился извините', 'Нихачу', 'Давай это, еще разок', 'нит.']
+# Warning when voice message is sent
+voiceWarns = ['ГОЛОСОВЫЕ ЗАПРЕЩЕНЫ В ЭТОМ ЧАТЕ', 'МОЖЕТ ХВАТИТ, А',
+              'Слушай, я скоро из чата кикать буду таких да']
+# Greeting when added to a chat
+helloMsgs = ['Здарова', 'Доброго времени суток :-)']
+# Error messages
+errorMsgs = ['Кхе кхе кххха ух подавился извините',
+             'Нихачу', 'Давай это, еще разок', 'нит.']
+# Cooldown messages
+cooldownMsgs = ['Таак падажжи', 'Не так быстро, парень',
+                'Ты не понимашь, что всех бесишь?']
